01adddd83d9d7910b6702545e9dba29ef714cb7f
MatasVal <valauskasmatas@gmail.com>
2022-06-06 21:27:49 +0300

    informacijos ivedimas

--- BasicMokymai/BasicMokymai/Program.cs
+++ BasicMokymai/BasicMokymai/Program.cs
@@ -19,6 +19,32 @@
             Console.WriteLine($"{"išvedimas"} {"vienoje"} {"eilutėje"}"); //interpoliacija
             Console.WriteLine("-------------");
 
+            Console.WriteLine("tekstas idedamas i \"kabutes\" ");
+            Console.WriteLine($"tekstas {Environment.NewLine}\nidedamas \r\ni \r\n\"kabutes\" ");
+            Console.WriteLine("tekstas \tidedamas \ti \t\"kabutes\" ");
+
+            
+            Console.WriteLine("----Press any key to continue------");
+            Console.ReadKey();
+            Console.Clear();
+
+            //Console.WriteLine("Įveskite savo vardą, o aš jį pakartosiu:");
+            //Console.WriteLine("o štai mano pakartojimas " + Console.ReadLine());
+            //Console.WriteLine("o štai mano pakartojimas {0}", + Console.ReadLine());
+            //Console.WriteLine($"o štai mano pakartojimas {0}  Console.ReadLine()");
+
+            //Console.WriteLine("Įveskite raidę");
+            //var key = Console.ReadKey();
+            //Console.WriteLine("ivestas simbolis {0}",  key.KeyChar);
+            //Console.WriteLine("ivestas simbolis {0}", key.Key);
+            //Console.WriteLine("ivestas simbolis {0}", (int)key.KeyChar);
+
+            //Console.WriteLine("ivestas simbolis {0}", Console.ReadKey().KeyChar);
+            //Console.WriteLine("ivestas simbolis {0}", (int)Console.ReadKey().KeyChar);
+
+            Console.WriteLine("Įveskite savo vardą, o as atspesiu pirma raide:");
+            Console.WriteLine("o štai mano spejimas \"" + Console.ReadLine()[0] + "\"");
+
             Console.ReadKey();
         }
     }
