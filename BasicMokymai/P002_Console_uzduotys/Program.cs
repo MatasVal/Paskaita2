fe738d59308a14528ba53740e48f680fd4efd032
MatasVal <valauskasmatas@gmail.com>
2022-06-07 20:26:40 +0300

    pirmosios uzduotys

---
+++ BasicMokymai/P002_Console_uzduotys/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace P002_Console_uzduotys
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("Matas");
+            //Console.WriteLine("   " + Console.ReadLine());
+
+            //Console.WriteLine("    ivestas ACII   " +  (int) Console.ReadKey() .KeyChar);
+
+            //Console.WriteLine("iveskite savo vardo pirmaja raide ir betkoki skaiciu");
+            //Console.WriteLine("   rezultatas yra: {0}", (int)Console.ReadKey().KeyChar + (int)Console.ReadKey().KeyChar);
+
+
+
+            //Console.WriteLine("(1) Pirkti");
+            //Console.WriteLine("(2) Parduoti");
+            //Console.WriteLine("(3) Likučiai");
+            //Console.WriteLine("    \"pasirinkimas yra : {0}\"", Console.ReadKey().KeyChar);
+
+            //parasykite programa kuri zodzio LABAS kiekviena raide isveda naujoje eiluteje
+            //Console.WriteLine("L\nA\nB\nA\nS");
+            /*Console.WriteLine(@"L
+A
+B
+A
+S");
+            Console.WriteLine("L");
+            Console.WriteLine("A");
+            Console.WriteLine("B");
+            Console.WriteLine("A");
+            Console.WriteLine("S");
+
+
+            //parasyti nutolusia per viena tab
+            Console.WriteLine("L\tA\tB\tA\tS");
+            */
+
+            //parasyti "LABAS"
+            //Console.WriteLine(" \"LABAS\" ");
+            //Console.WriteLine(" \u0022LABAS\u0022 ");
+
+
+
+            //Console.WriteLine("parasykite savo varda o as atspesiu jo antra raide");
+            // Console.WriteLine("   " + Console.ReadLine()[1]);
+
+            //parasykite o as atspesiu raidziu kieki
+            //Console.WriteLine("   " + Console.ReadLine().Length); 
+
+            //parasyti ivesta zodi ir skaiciu formatu "|    matas|   5|"
+            //Console.WriteLine("|     {0}  |           {1}|", Console.ReadLine(), Console.ReadLine());
+
+
+            //Console.WriteLine("\n\n\n{0}", Console.ReadLine());
+
+
+            /*Console.WriteLine("       **************");
+            Console.WriteLine("     **              **");
+            Console.WriteLine("    **    ()     ()   **");
+            Console.WriteLine("   **         |        **");
+            Console.WriteLine("  **          |         **");
+            Console.WriteLine("   **         |        **");
+            Console.WriteLine("    **     |-----|    **");
+            Console.WriteLine("      **             **");
+            Console.WriteLine("       **************");
+
+            .Replace("*", "\"")
+            */
+
+
+
+        }
+    }
+}
