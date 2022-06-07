baa79981b495697e6bbf1ad8ab0de94a52196cd9
MatasVal <valauskasmatas@gmail.com>
2022-06-07 22:08:13 +0300

    hanoi

---
+++ BasicMokymai/P002_stringKintamieji/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace P002_stringKintamieji
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("String (tekstinio) tipo kintamieji");
+            string kintamasis = "Hello World";
+            Console.WriteLine(kintamasis);
+
+            var stringKintamasis = "bet koks tekstas";
+            string tuscias = "";
+            string nulas = null;
+            string Laisvaerdve = "    ";
+
+            string tekstas = "";
+
+            string konkatinacija = stringKintamasis + kintamasis;
+            Console.WriteLine(konkatinacija);
+            string kompozicija = string.Format("{0}", stringKintamasis);
+            string interpoliacija = $"{stringKintamasis}";
+
+            kintamasis = "tekstas betkoks";
+            Console.WriteLine(kintamasis);
+
+
+
+
+
+
+        }
+    }
+}
