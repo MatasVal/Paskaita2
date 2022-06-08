57b85e3e2d3b74b493381df32ed92e0a7a3f8f19
MatasVal <valauskasmatas@gmail.com>
2022-06-08 22:06:48 +0300

    bandymai

--- BasicMokymai/P003_Kintamieji/Program.cs
+++ BasicMokymai/P003_Kintamieji/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Sveiki, kintamieji");
+            /*Console.WriteLine("Sveiki, kintamieji");
             //Skaiciu kintamieji
             //Sveiku skaiciu kintamieji
             byte mazasSkaicius = 2; //255 max
@@ -45,7 +45,58 @@
 
 
             Console.WriteLine("char kintamieji");
-            char raide = 'n'; //yra sveiko skaitmens tipas, nors atrodo kad talpina raide. integer ar ushort analogas.
+            char raide = 'n'; //yra sveiko skaitmens tipas, nors atrodo kad talpina raide. integer ar ushort analogas.*/
+
+
+
+            //PARAŠYTI PROGRAMĄ KURIOJE SAUGOME :
+            //• MOKYKLOS PAVADINIMĄ
+
+            var mokyklosPavadinimas = "CodeAcademy";
+            var kursoPavadinimas = "CA.NET2";
+            var studentuSkaicius = "19";
+            var siandienosData = new DateTime(2022, 06, 08);
+
+            Console.WriteLine($"mokyklosPavadinimas - {mokyklosPavadinimas}\nkurso pavadinimas - {kursoPavadinimas}\nstudentu skaicius - {studentuSkaicius}\nsiandienos data - {siandienosData}");
+
+            var kursuPradzia = new DateTime(2022, 05, 30);
+            var kursuPabaiga = new DateTime(2022, 12, 26);
+            var kursoTrukme = 
+            TimeSpan diff = kursuPabaiga - kursuPradzia;
+            Console.WriteLine("skirtumas: {0}", diff);
+
+
+
+            //Sukurkite tris kintamuosius.tekstinio, sveiko skaitmens ir loginio tipo.
+            //Parašykite programą kuri į konsolę visus aprašytus kintamuosius vienoje eilutėje atskiriant tarpu
+
+            string tekstas = "tekstas";
+
+            bool
+                 var tekstinio
+
+
+
+
+
+            //Console.WriteLine("CodeAcademy");
+
+
+            //• KURSO PAVADINIMĄ
+            //Console.WriteLine(".NET");
+
+            //var kursoPavadinimas = .NET;
+
+
+            //• STUDENTŲ SKAIČIŲ
+            //Console.WriteLine("19");
+
+            //• ŠIANDIENOS DATĄ
+            //Console.WriteLine(DateTime.Now);
+
+            //• VISUS KINTAMUOSIUS IŠVESTI Į EKRANĄ 
+
+
 
 
         }
