9d235efecfde19fa3c574de86fbb874d80c05727
MatasVal <valauskasmatas@gmail.com>
2022-06-14 22:00:00 +0300

    pusiau

---
+++ BasicMokymai/P005_operatoriai/Program.cs
@@ -0,0 +1,191 @@
+﻿
+Console.WriteLine("Hello, Operatoriai!");
+
+//Reliaciniai operatoriai == != > < >= <=
+
+/*var skaicius = 10;
+var nelyginisSkaicius = 5;
+var lyginisSkaicius = 10;
+Console.WriteLine(" == patikrina ar kintamieji yra lygus");
+Console.WriteLine($"{skaicius}=={lyginisSkaicius} yra {skaicius == lyginisSkaicius}");
+
+bool ar10yraLygu5 = skaicius == nelyginisSkaicius;
+Console.WriteLine($"{skaicius}=={nelyginisSkaicius} yra {ar10yraLygu5}");
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("!=patikrina ar kintamieji yra nelygus");
+Console.WriteLine($"{skaicius}!={lyginisSkaicius} yra {skaicius != lyginisSkaicius}");
+Console.WriteLine($"{skaicius}!={nelyginisSkaicius} yra {skaicius != nelyginisSkaicius}");
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("> patikrina ar kaireje esanti reiksme yra didesne uz desineje esancia");
+Console.WriteLine($"{skaicius}>{lyginisSkaicius} yra {skaicius > lyginisSkaicius}");
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("> patikrina ar kaireje esanti reiksme yra mazesneuz desineje esancia");
+Console.WriteLine($"{skaicius}<{lyginisSkaicius} yra {skaicius < lyginisSkaicius}");
+Console.WriteLine($"{skaicius}<{nelyginisSkaicius} yra {skaicius < nelyginisSkaicius}");
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("> patikrina ar kaireje esanti reiksme yra didesne arba lygi uz desineje esancia");
+Console.WriteLine($"{skaicius}>={lyginisSkaicius} yra {skaicius >= lyginisSkaicius}");
+Console.WriteLine($"{skaicius}>={nelyginisSkaicius} yra {skaicius >= nelyginisSkaicius}");
+
+Console.WriteLine("-------------------------------------");
+Console.WriteLine("> patikrina ar kaireje esanti reiksme yra mazesne arba lygi uz desineje esancia");
+Console.WriteLine($"{skaicius}<={lyginisSkaicius} yra {skaicius <= lyginisSkaicius}");
+Console.WriteLine($"{skaicius}<={nelyginisSkaicius} yra {skaicius <= nelyginisSkaicius}");
+
+
+//var a = "A";
+//var b = "B";
+//Console.WriteLine($"{a}>{b} yra {a > b}");
+
+sukurkite naują kintamajį long ir prskirkite didžiausią reikšmę.
+sukurkite naują kintamajį short ir prskirkite didžiausią reikšmę
+- padalinkite didesnį skaičių iš mažesnio
+- iš rezultato atimkite maksimalų long skaičių
+- ir pridėkite maksimalų int skaičių
+*/
+
+/*long maxLong = 9223372036854775807;//galima ir per long.maxvalue
+short maxShort = 32767;
+var rezultatas3 = (double)maxLong / maxShort;
+
+Console.WriteLine($"padalinkite didesni skaiciu is mazesnio {rezultatas3}" );
+
+var rezultatas31 = rezultatas3 - maxLong;
+Console.WriteLine($"is rezultato atimkite max long skaiciu {rezultatas31}");
+
+Console.WriteLine($"ir pridekite maksimalu int skaiciu {rezultatas31 + int.MaxValue}");
+
+
+
+PARAŠYTI PROGRAMĄ KURI DIDELĮ 10 ŽENKLĮ SKAIČIŲ DOUBLE, KONVERTUOJA Į
+INT , SHORT , BYTE
+STEBĖTI REZULTATĄ.
+*/
+
+
+//double didelis10 = Convert.ToDouble(Console.ReadLine());
+//Console.WriteLine($" int = {(int)didelis10}");
+//Console.WriteLine($" short = {(short)didelis10}");
+//Console.WriteLine($" byte = {(byte)didelis10}");
+
+
+//Console.WriteLine( Convert.ToInt32(didelis10);
+
+
+//PARAŠYTI PROGRAMĄ KURI
+//PRAŠO ĮVESTI RUTULIO DIAMETRĄ,
+//o IŠVEDA PLOTĄ IR TŪRĮ
+
+/*
+Console.WriteLine("irasykite rutulio diametra, apskaiciuosime plota ir turi");
+
+var sphereDiameter = double.Parse(Console.ReadLine());
+var sphereRadius = sphereDiameter / 2;
+var sphereArea = 4 * 3.14 * sphereRadius * sphereRadius;
+var spherevolume = (4 / 3) * 3.14 * (sphereRadius * sphereRadius * sphereRadius);
+*/
+
+/*PARAŠYTI PROGRAMĄ KURI PRAŠO ĮVESTI ATSTUMĄ (METRAIS) IR LAIKĄ(SEKUNDĖMIS),
+-IŠVEDA GREITĮ km/h.
+- IŠVEDA GREITĮ km/s.*/
+
+/*Console.WriteLine("iveskite atstuma metreais");
+var atstumas = double.Parse(Console.ReadLine());
+var laikas = double.Parse(Console.ReadLine());
+
+var metruKiekisKilometre = 
+var sekKiekisValandoje = 
+
+Console.WriteLine();
+
+var greitisKMPerH = atstumas
+var greitisKMPerS =
+*/
+
+
+
+/*Nuskaitykite iš klaviatūros 2 skaičius (x ir y).
+Išveskite į ekraną funkciją y+2y+x+1 ir apskaičiuokite šios funkcijos rezultatą.
+Išveskite į ekraną funkciją y²+x/2 apskaičiuokite šios funkcijos rezultatą.
+
+Console.WriteLine("iveskite x");
+var x = int.Parse(Console.ReadLine());
+Console.WriteLine("iveskite y");
+
+var y = int.Parse(Console.ReadLine());
+
+Console.WriteLine($" y+2y+x+1 = {y + 2 * y + x + 1}");
+Console.WriteLine($" y*y +x/2 = {y * y + x / 2}");*/
+
+
+/*PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
+VISUS ĮVESTUS 1 PAKEISKITE Į 0 IR GAUTĄ SKAIČIŲ PADALINKITE IŠ 2
+PVZ BUVO ĮVESTA 12345
+REZULTATAS 2345/2=1172,5
+<HINT> naudokite replace
+*/
+
+/*
+ Console.WriteLine("iveskite penkiazenkli sveikaji skaiciu:");
+
+var skaicius = Convert.ToDouble(Console.ReadLine().Replace("1", "0"));
+Console.WriteLine($"{skaicius}/2={skaicius/2}");
+
+
+
+
+
+PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI PENKIAŽENKLĮ SVEIKĄ SKAIČIŲ
+VISUS ĮVESTUS išskyrus 1 PAKEISKITE Į 0 IR GAUTĄ SKAIČIŲ PADALINKITE IŠ 2
+PVZ BUVO ĮVESTA 12345
+REZULTATAS 10000/2=5000
+<HINT> naudokite replace
+*/
+/*
+
+Console.WriteLine("iveskite penkiazenkli sveikaji skaiciu:");
+
+var skaicius = Convert.ToDouble(Console.ReadLine()//is konsoles nuskaitau stringa
+    .Replace("2", "0")
+    .Replace("3", "0")
+    .Replace("4", "0")
+    .Replace("5", "0")
+    .Replace("6", "0")
+    .Replace("7", "0")
+    .Replace("8", "0")
+    .Replace("9", "0")
+    );
+Console.WriteLine($"{skaicius}/2={skaicius / 2}");
+
+ 
+ 
+
+
+ PARAŠYTI PROGRAMĄ KURI NAUDOTOJO PAPRAŠO ĮVESTI SVEIKĄ SKAIČIŲ
+VIENOJE EILUTĖJE IŠVESKITE ŠĮ SKAIČIŲ 5 KARTUS VIS PADIDINAMI VIENETU.
+PVZ BUVO ĮVESTA 5
+REZULTATAS 6 7 8 9 10
+*/
+
+/*Console.WriteLine("iveskite sveika skaiciu:");
+
+var skaicius = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($" rezultatas {++skaicius}{++skaicius}{++skaicius}{++skaicius}{++skaicius}");
+
+*/
+
+
+
+
+
+
+
+
+
+
+
---
+++ BasicMokymai/Termometras/Program.cs
@@ -0,0 +1,74 @@
+﻿
+//*Paprašykite naudotojo įvesti 1 skaičių - temperatūrą pagal Celsijų. 
+Console.WriteLine("prasome ivesti viena skaiciu:");
+var celsius = Convert.ToDouble(Console.ReadLine());
+
+//- Paskaičiuokite ir išveskite į ekraną temperatūrą pagal farenheitą.
+
+var farenheitas = (celsius * 9 / 5) + 32;
+var kelvinas = celsius + 273.15;
+
+
+Console.WriteLine($"Farenheitas ={(celsius*9/5) +32}");
+
+//- Paskaičiuokite ir išveskite į ekraną temperatūrą pagal kelviną.
+Console.WriteLine($"Kelvinas ={celsius+273.15}");
+
+//- Gautą temperatūrą pagal farenheitą perskaičiuokite į Celsijų ir patikrinkite ar sutampa su įvestu skaičių (išveskite true/false)
+Console.WriteLine($"konvertuojamas farenheitas atgal i celsiju = {(farenheitas -32) /1.8} ");
+
+bool vertimas0 = (farenheitas - 32) / 1.8 == celsius;
+Console.WriteLine($"ar sutampa farenheitas su celsijumi - {vertimas0} ");
+
+//-Gautą temperatūrą pagal kelviną perskaičiuokite į celsijų ir patikrinkite ar sutampa su įvestu skaičiu (išveskite true/false)
+Console.WriteLine($"konvertuojamas kelvinas atgal i celsiju = {kelvinas - 273.15} ");
+
+bool vertimas1 = kelvinas - 273.15 == celsius;
+
+Console.WriteLine($"ar sutampa kelvinas su celsijumi - {vertimas1} ");
+//-Paskaičiuotą temperatūrą pagal farenheita peverskite į kelviną ir patikrinkite ar sutampa su ankstesniais skaičiavimais (išveskite true/false)
+
+
+bool vertimas2 = (farenheitas - 32) / 1.8 + 273.15 == vertimas0 == vertimas1;
+
+
+//-Nupieškite termometrą pagal Celsijų 
+//a) Atvaizduokite skalę, sugraduotą kas 5 laipsnius C priklausomai nuo įvestos temperatūros pridedant ir atimant 40 laipsnių 
+//(tarkime įvesta buvo 10, tuomet skalė bus nuo -30 iki +50)
+// b) Grafiškai atvaizduokite įvestą temperatūros stulpelį. 
+//<HINT> naudokite .ToString(), palyginimo reliacinius operatorius (==, >, < ir t.t.) ir.Replace().
+// if naudoti negalima, ternary operator (?) naudoti negalima.
+
+
+
+
+
+
+
+/*rezultatas gali atrodyti taip:
+                            |--------------------|
+                            |   ^F _    ^C   |
+                            |  100  - | | -  40  |
+                            |   95  - | | -  35  |
+                            |   90  - | | -  30  |
+                            |   80  - | | -  25  |
+                            |   70  - | | -  20  |
+                            |   60  - | | -  15  |
+                            |   50  - |#| -  10  |
+                            |   40  - |#| -   5  |
+                            |   30  - |#| -   0  |
+                            |   20  - |#| -  -5  |
+                            |   10  - |#| - -10  |
+                            |    5  - |#| - -15  |
+                            |    0  - |#| - -20  |
+                            |  -10  - |#| - -25  |
+                            |  -20  - |#| - -30  |
+                            |  -30  - |#| - -35  |
+                            |  -40  - |#| - -40  |
+                            |        '***`       |
+                            |       (*****)      |
+                            |        `---'       |
+                            |____________________|
+
+*/
+
