b585dcbf80325dcba9b9e348484e561864293376
MatasVal <valauskasmatas@gmail.com>
2022-06-28 12:51:33 +0300

    baigtas

--- BasicMokymai/P009_AmziausMelagis/Program.cs
+++ BasicMokymai/P009_AmziausMelagis/Program.cs
@@ -1,78 +1,17 @@
-﻿/*
- Sukurkite programą, kuri pateiktų vartotojo registracijos formą.
-             Vartotojas įveda:
-              -vardą ir pavardę
-              - asmens kodą (11simb.)
-              -amžių(sveiką skaičių metais) ir / arba gimimo datą(galima abu, tik kažkurį vieną, ar neįvesti nei vieno)
-            Programa į ekraną išveda ataskatą:
-             -šiandienos datą
-             - Vardas, pavardė
-             - Lytis
-                < HINT > asmens kodo pirmasis rodo gimimo šimtmetį ir asmens lytį
-                (1 – XIX a. gimęs vyras,
-                 2 – XIX a. gimusi moteris,
-                 3 – XX a. gimęs vyras,
-                 4 – XX a. gimusi moteris,
-                 5 – XXI a. gimęs vyras,
-                 6 – XXI a. gimusi moteris
-                 tolesni šeši:
-                      metai(du skaitmenys),
-                      mėnuo(du skaitmenys),
-                      diena(du skaitmenys))    
-             -Asmens kodas
-                < NEPRIVALOMAS PASUNKINIMAS > asmens kodas validuojamas pagal tokias salygas
-                   Paskaičiuojamas Kontrolinis skaičius
-                   a) jei kontrolinis skaičius teisingas išvedamas asmens kodas
-                   b) jei neteisingas išvedamas tekstas "kodas neteisingas",
-                      o laukeAmžiaus patikimumas išvedama "patikimumui trūksta duomenų"
-                      <HINT> https://lt.wikipedia.org/wiki/Asmens_kodas
-             -Amžius
-             - Amžiaus patikimumas - pagal tokias sąlygas:
-             -jei įvestas amžius metais, paskaičiuoti gimimo metus ir sulyginti su įvestu asmens kodu.
-                a) jei sutampa išvesti "amžius patikimas"
-                b) jei nesutampa išvesti "amžius pameluotas"
-             - jei įvesta gimimo data sulyginti su įvestu asmens kodu.
-                a) jei sutampa išvesti "amžius patikimas"
-                b) jei nesutampa išvesti "amžius pameluotas"
-             - jei įvesta ir amžius ir gimimo data sulyginti su įvestu asmens kodu.
-                a) jei viskas sutampa išvesti "amžius tikras"
-                b) jei asmens kodu sutampa tik vienas įvestų, išvesti "amžius nepatikimas"
-                c) jei nesutampa išvesti "amžius pameluotas"
-             - jei neįvesta nei amžius nei gimimo data išvesti
-                a) "patikimumui trūksta duomenų"
-
-
-
-Rezultatas gali atrodyti taip:
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ATASKAITA APIE ASMENĮ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓      2022-06-20       ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓     Vardas, pavardė ▓ Vardenis Pavardenis                 ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓               Lytis ▓ Vyras                               ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓        Asmens kodas ▓ 44012029286                         ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓              Amžius ▓ 82                                  ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓         Gimimo data ▓ 1980-06-20                          ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓ Amžiaus patikimumas ▓ amžius nepatikimas                  ▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-*/
-
+﻿
 
 Console.WriteLine("Prasome ivesti savo varda ir pavarde:");
 string vardasPavarde = Console.ReadLine();
 Console.WriteLine();
 
+
+
 Console.WriteLine("Prasome ivesti savo asmens koda (11 simb.):");
 var asmensKodoIvestis = Console.ReadLine();
 long asmensKodas;
 bool arAsmensKodasYraSkaicius = long.TryParse(asmensKodoIvestis, out asmensKodas); //patikrinu ar asmens kodas yra skaicius
+
+
 
 if (arAsmensKodasYraSkaicius) 
 {
@@ -92,19 +31,30 @@ else
     Console.WriteLine("Asmens koda turi sudaryti vien skaitmenys"); //jeigu asm kodas yra sudarytas ne is skaitmenu ar ne tik is skaitmenu, darbas nutraukiamas
     Environment.Exit(0);
 }
+
+
 Console.WriteLine();
 
 Console.WriteLine("Prasome ivesti savo amziu:");
 
-var amzius = Convert.ToInt32(Console.ReadLine());
+string amziausIvestis = Console.ReadLine();
+
+bool arAmziusYraIvestas = int.TryParse(amziausIvestis, out int amzius);
+
 
 Console.WriteLine();
 
 Console.WriteLine("Prasome ivesti savo gimimo data:");
 
-var gimimoDataIvesta = DateTime.Parse(Console.ReadLine());
- 
-var gimimoDataSuformatuota = gimimoDataIvesta.ToString("yyyy-MM-dd"); //pasidarau kintamaji kuris yra suformatuota gimimo data
+
+
+var gimimoDataIvestis = Console.ReadLine();
+
+bool arGimimoDataYraIvesta = DateTime.TryParse(gimimoDataIvestis, out DateTime gimimoData);
+
+string gimimoDataSuformatuota = gimimoData.ToString("yyyy-MM-dd"); //pasidarau kintamaji kuris yra suformatuota gimimo data
+
+
 
 Console.WriteLine();
 
@@ -112,11 +62,190 @@ var siandienosDataSuformatuota = DateTime.Now.ToString("yyyy-MM-dd");
 
 string lytis = (asmensKodas >= 10000000000 && asmensKodas <= 19999999999 || asmensKodas >= 30000000000 && asmensKodas <= 39999999999 || asmensKodas >= 50000000000 && asmensKodas <= 59999999999) ? "Vyras" : "Moteris";
 
-var gimimoMetaiIsskaiciuoti =
-if (amzius) 
+string asmensKodasString = asmensKodas.ToString();
 
-bool arAmziusYraIvestas = string.IsNullOrEmpty(amzius);
+string isAsmensKodoIskirptaGimimoData = asmensKodasString.Substring(1, 6);
+string asmensKodoPirmasisSkaitmuo = asmensKodasString.Substring(0, 1);
 
+//jei įvestas amžius metais, paskaičiuoti gimimo metus ir sulyginti su įvestu asmens kodu.
+//  a) jei sutampa išvesti "amžius patikimas"
+//  b) jei nesutampa išvesti "amžius pameluotas"
+
+var einamiejiMetai = DateTime.Now.ToString("yyyy");
+
+int einamiejiMetaiInt = Convert.ToInt32(einamiejiMetai);
+
+var gimimoMetaiIsAmziaus = einamiejiMetaiInt - (amzius+1);
+
+string gimimoMetaiIsAmziausString = Convert.ToString(gimimoMetaiIsAmziaus);
+
+//Is asmens kodo issivesta gimimo data
+
+string issivestaGimimoData19amzius = ("18" + isAsmensKodoIskirptaGimimoData); //jei asm kodas prasideda vienetu, tai zmogus gimes 18** metais, pridedu "18" prie trumpintos gim datos
+string issivestaGimimoData20amzius = ("19" + isAsmensKodoIskirptaGimimoData);
+string issivestaGimimoData21amzius = ("20" + isAsmensKodoIskirptaGimimoData);
+string issivestaGimimoDataSuBruksniais19amzius = issivestaGimimoData19amzius.Insert(4, "-").Insert(7, "-");
+string issivestaGimimoDataSuBruksniais20amzius = issivestaGimimoData20amzius.Insert(4, "-").Insert(7, "-");
+string issivestaGimimoDataSuBruksniais21amzius = issivestaGimimoData21amzius.Insert(4, "-").Insert(7, "-");
+string issivestiGimimoMetai19amzius = issivestaGimimoData19amzius.Substring(0, 4);
+string issivestiGimimoMetai20amzius = issivestaGimimoData20amzius.Substring(0, 4);
+string issivestiGimimoMetai21amzius = issivestaGimimoData21amzius.Substring(0, 4);
+string amziausPatikimumas= "";
+
+
+//jei amzius ivestas, ziurima koks pirmas skaicius ir nuo to priklausomai pridedamas amzius nepilnos prie gimimo datos,
+//Gauta papildyta  data lyginama su gauta data skaiciuojant nuo ivesto amziaus, priklausomai nuo to ar sutampa yra isvedamas patikimumas
+
+
+
+if (arAmziusYraIvestas)
+{
+    if (asmensKodoPirmasisSkaitmuo == "1" || asmensKodoPirmasisSkaitmuo == "2")
+    {
+
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai19amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "3" || asmensKodoPirmasisSkaitmuo == "4")
+    {
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai20amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "5" || asmensKodoPirmasisSkaitmuo == "6")
+    {
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai21amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+}
+
+//Jei ivesta gimimo data ji yra lyginama su asmens kodu
+
+if (arGimimoDataYraIvesta)
+{
+    if (asmensKodoPirmasisSkaitmuo == "1" || asmensKodoPirmasisSkaitmuo == "2")
+    {
+
+        if (gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais19amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "3" || asmensKodoPirmasisSkaitmuo == "4")
+    {
+        if (gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais20amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "5" || asmensKodoPirmasisSkaitmuo == "6")
+    {
+        if (gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais21amzius)
+        {
+            amziausPatikimumas = "amzius patikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+}
+
+//jei įvestas ir amžius ir gimimo data sulyginti su įvestu asmens kodu
+
+if (arGimimoDataYraIvesta && arAmziusYraIvestas)
+{
+    if (asmensKodoPirmasisSkaitmuo == "1" || asmensKodoPirmasisSkaitmuo == "2")
+    {
+
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai19amzius && gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais19amzius)
+        {
+            amziausPatikimumas = "amzius tikras";
+        }
+        else if (gimimoMetaiIsAmziausString == issivestiGimimoMetai19amzius || gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais19amzius)
+        {
+            amziausPatikimumas = "amzius nepatikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "3" || asmensKodoPirmasisSkaitmuo == "4")
+    {
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai20amzius && gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais20amzius)
+        {
+            amziausPatikimumas = "amzius tikras";
+        }
+        else if (gimimoMetaiIsAmziausString == issivestiGimimoMetai20amzius || gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais20amzius)
+        {
+            amziausPatikimumas = "amzius nepatikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+    else if (asmensKodoPirmasisSkaitmuo == "5" || asmensKodoPirmasisSkaitmuo == "6")
+    {
+        if (gimimoMetaiIsAmziausString == issivestiGimimoMetai21amzius && gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais21amzius)
+        {
+            amziausPatikimumas = "amzius tikras";
+        }
+        else if (gimimoMetaiIsAmziausString == issivestiGimimoMetai21amzius || gimimoDataSuformatuota == issivestaGimimoDataSuBruksniais21amzius)
+        {
+            amziausPatikimumas = "amzius nepatikimas";
+        }
+        else
+        {
+            amziausPatikimumas = "amzius pameluotas";
+        }
+    }
+}
+
+// jei neįvestas nei amžius nei gimimo data
+
+if (!arGimimoDataYraIvesta && !arAmziusYraIvestas)
+{
+    amziausPatikimumas = "patikimumui trūksta duomenų";
+}
+
+if (!arGimimoDataYraIvesta)
+{
+    gimimoDataSuformatuota = "neivesta";
+}
+
+string amziusString = Convert.ToString(amzius);
+
+if (!arAmziusYraIvestas)
+{
+    amziusString = "neivesta";
+}
 
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓ ATASKAITA APIE ASMENĮ ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
@@ -125,14 +254,14 @@ Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
 Console.WriteLine($"▓     Vardas, pavardė ▓ {vardasPavarde,-30}      ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
-Console.WriteLine($"▓               Lytis ▓ {lytis,-7}                               ▓");
+Console.WriteLine($"▓               Lytis ▓ {lytis,-7}                             ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
 Console.WriteLine($"▓        Asmens kodas ▓ {asmensKodas,11}                         ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
-Console.WriteLine($"▓              Amžius ▓ {amzius,2}                                  ▓");
+Console.WriteLine($"▓              Amžius ▓ {amziusString,-8}                            ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
-Console.WriteLine($"▓         Gimimo data ▓ {gimimoDataSuformatuota,10}                          ▓");
+Console.WriteLine($"▓         Gimimo data ▓ {gimimoDataSuformatuota,-10}                          ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
-Console.WriteLine($"▓ Amžiaus patikimumas ▓ amžius nepatikimas                  ▓");
+Console.WriteLine($"▓ Amžiaus patikimumas ▓ {amziausPatikimumas,-30}      ▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
 Console.WriteLine($"▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓");
---
+++ BasicMokymai/P010_Methods/Program.cs
@@ -0,0 +1,306 @@
+﻿namespace P010_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Methods!");
+            Console.WriteLine("Sukuriame pirma savo metoda");
+
+            IsveskKazkaEkranan(); //metodo kvietimas, kai metodas nieko negrazina ir nieko nepriima
+
+            Console.WriteLine("--------------------------");
+
+            float kazkoksSkaicius = GautiAtsitiktiniSkaiciu(); //metodo kvietimas ir grazinamos reiksmes priskyrimas
+            Console.WriteLine($"kazkoksSkaicius = {kazkoksSkaicius}");
+
+            Console.WriteLine("--------------------------");
+            int a = 2;
+
+            IsveskSkaiciuEkranan(a); //metodas su vienu parametru
+            Console.WriteLine($"skaicius a is Main {a}"); // reiksme nesikeicia is Main
+            Console.WriteLine("--------------------------");
+
+            int sk1 = 2;
+            int sk2 = 2;
+            int sudaugintuDuSkaiciai = DaugintiSkaicius(sk1, sk2);
+
+            Console.WriteLine($"sudaugintuDuSkaiciai = {sudaugintuDuSkaiciai}"); //metodas su dviem parametrais grazinantis int
+
+            Console.WriteLine("--------------------------");
+
+            int sudaugintiTrysSkaiciai = DaugintiSkaicius(sk1, sk2, 2);
+            Console.WriteLine($"sudaugintiTrysSkaiciai = {sudaugintiTrysSkaiciai}"); //metodo overloadinimas, metodas priima 3 argumentus ir grazina int
+
+            Console.WriteLine("--------------------------");
+
+            double sk1d = 2.1;
+            double sk2d = 2.1;
+            double sudaugintiDuDoubleSkaiciai = DaugintiSkaicius(sk1d, sk2d);
+            Console.WriteLine($"sudaugintiDuDoubleSkaiciai = {sudaugintiDuDoubleSkaiciai}");
+
+            Console.WriteLine("--------------------------");
+
+            double sudaugintiDuDoubleSkaiciai1 = DaugintiSkaicius((double)sk1, sk2d); //auto daromas castinimas, bet daromas explicit kad butu geresnis skaitomumas
+            Console.WriteLine($"sudaugintiDuDoubleSkaiciai = {sudaugintiDuDoubleSkaiciai1}");
+            Console.WriteLine("--------------------------");
+
+            IsveskTekstaEkranan();
+            IsveskTekstaEkranan("kazkoks tekstas");
+
+
+            Console.WriteLine("--------------------------");
+
+            IsveskSkaiciuIrTekstaEkranan(1);
+            IsveskSkaiciuIrTekstaEkranan(1, "kazkoks tekstas");
+
+            Console.WriteLine("--------------------------");
+
+            int patikrintasSkaicius = SkaiciausPatikrinimas(20, 50, 100);
+
+            Console.WriteLine($"patikrintasSkaicius = {patikrintasSkaicius}");
+
+
+            Console.WriteLine("--------------------------");
+
+            int patikrintasSkaicius1 = SkaiciausPatikrinimas(max: 100, min: 50, skaicius: 51); //gerokai skaitomesnis kvietimas per named parametrus
+            Console.WriteLine($"patikrintasSkaicius1 = {patikrintasSkaicius1}");
+
+            Console.WriteLine("--------------------------");
+
+            Console.WriteLine("vidurkis" + Vidurkis(2, 3));
+            Console.WriteLine("vidurkis1" + Vidurkis(2, 3, 8));
+            Console.WriteLine("vidurkis2" + Vidurkis(2, 3, 654, 545, 45, 54, 54, 555, 666));
+
+
+            Console.WriteLine("--------------------------");
+
+            GautiSkaiciu(out int gautasSkaicius);
+            Console.WriteLine($"gautasSkaicius = {gautasSkaicius}");
+
+            Console.WriteLine("--------------------------");
+
+
+            int rsk = 2;
+            Console.WriteLine($"rsk = {rsk}");
+            ReferenceSkaicius(ref rsk); //reiksmes perdavimas per reference keicia reiksme kviecianciame metode
+            Console.WriteLine($"po ReferenceSkaicius rsk = {rsk}");
+
+            Console.WriteLine("--------------------------");
+
+
+            //lokalios funkcijos
+
+            int Add(int a, int b)
+            {
+                return a + b;
+            }
+
+            Console.WriteLine(Add(2, 2));
+
+            //geriau nenaudoti, bet yra
+
+
+            /* Parašykite programą kurioje yra vienas metodas priimantis du skaitmeninio tipo argumentus.
+ - Main metode naudotojo paprašome įvesti 2 skaičius ir perduokite juos metodui.
+ N.B.būtina validacija
+ - Į ekraną išveskite argumentų matematinę sumą.
+ Pvz:
+ > Iveskite pirmą skaičių:
+ _ 15
+ > Iveskite antrą skaičių:
+ _ 16
+ > Rezultatas: 31*/
+
+            Console.WriteLine("Iveskite pirma skaiciu: ");
+            string skA1 = Console.ReadLine();
+            Console.WriteLine("Iveskite antra skaiciu: ");
+            string skA2 = Console.ReadLine();
+
+            bool arSkA1 = int.TryParse(skA1, out int intSkA1);
+            bool arSkA2 = int.TryParse(skA2, out int intSkA2);
+
+            if (arSkA1 && arSkA2)
+            {
+                MatematineSuma(intSkA1, intSkA2);
+            }
+            else
+            {
+                Console.WriteLine("Ivesta blogai");
+            }
+
+
+
+        }
+
+
+
+        public static void MatematineSuma(int intSkA1, int intSkA2)
+        {
+            Console.WriteLine($"{intSkA1} + {intSkA2}={intSkA1 + intSkA2}");
+        }
+
+
+
+        //Parašykite programą kurioje yra 2 metodai.
+//- Pirmas metodas į konsolę išveda "Sveiki visi"
+//- Antrtas metodas į konsolę išveda "Linkiu jums geros dienos"
+
+
+        public static void SveikiVisi()
+        {
+            Console.WriteLine("sveikiVisi");
+        }
+
+       
+        public static void Linkiu()
+        {
+            Console.WriteLine("Linkiu jums geros dienos");
+        }
+
+        /*Parašykite programą kurioje yra 2 metodai.
+-
+- Pirmas metodas naudotojo paprašo įvesti savo vardą konsolę išveda "Labas tai_kas_ivesta" ir grąžina tai kas ivesta.
+- Antras metodas į konsolę išveda "Linkiu jums tai_kas_ivesta geros dienos"
+Pvz:
+> Iveskite savo Varda:
+_ Petras
+> Labas Petras
+> Linkiu jums Petras geros dienos*/
+
+        public static string NuskaitytiIrIsvestiVarda()
+        {
+            Console.WriteLine("Iveskite savo varda");
+            var vardas = Console.ReadLine();
+            
+            Console.WriteLine($"Labas {vardas}");
+                return vardas;
+        }
+
+        public static void Linkiu1()
+        {
+            Console.WriteLine($"Linkiu jums {NuskaitytiIrIsvestiVarda()} geros dienos");
+
+        }
+
+                
+        /*Parašykite programą kurioje yra vienas metodas priimantis vieną argumentą.
+- Main metode naudotojo paprašome įvesti betkokį tekstą su tarpais
+- Įvestas tekstas metodui perduodamas per parametrus ir grąžina tarpų kiekį
+- Main metode į ekraną išveskite tarpų kiekį
+Pvz:
+> Iveskite teksta:
+_ as mokausi programuoti
+> Tarpu kiekis yra: 2
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public static void ReferenceSkaicius(ref int skaicius)
+        {
+            skaicius = 900;
+        }
+
+
+
+
+        public static void GautiSkaiciu(out int skaicius)
+        {
+            skaicius = 2;
+        }
+
+
+
+
+
+
+        public static double Vidurkis(params int[] skaiciai)
+        {
+            double total = 0;
+            foreach (var skaicius in skaiciai)
+            {
+                total += skaicius;
+            }
+            return total/skaiciai.Length;
+        }
+
+
+        public static int SkaiciausPatikrinimas(int skaicius, int min, int max)
+        {
+            if (skaicius < min) 
+            {
+                return min;
+            }
+            if (skaicius > max)
+            {
+                return max;
+            }
+            return skaicius;
+
+
+        }
+
+
+
+
+
+        public static void IsveskTekstaEkranan(string tekstas = "tekstas neivestas")
+        {
+            Console.WriteLine("tekstas - " + tekstas);
+        }
+
+        public static void IsveskSkaiciuIrTekstaEkranan(int skaicius, string tekstas = "tekstas neivestas")
+        {
+            Console.WriteLine($"skaicius - {skaicius}, tekstas - {tekstas}");
+        }
+
+
+
+        public static double DaugintiSkaicius(double sk1d, double sk2d) //dar karta oveloadintas metodas, bet dabar jau pagal metodo tipa
+        {
+            return sk1d * sk2d;
+        }
+
+        public static int DaugintiSkaicius(int sk1, int sk2, int sk3)
+        {
+            return sk1 * sk2 * sk3;   
+        }
+
+        public static int DaugintiSkaicius(int sk1, int sk2)
+        {
+            return sk1 * sk2;   
+        }
+
+        public static void IsveskSkaiciuEkranan(int a)
+        {
+            a = a + 8;
+            Console.WriteLine($"skaicius yra {a}");
+        }
+
+        public static float GautiAtsitiktiniSkaiciu()
+        {
+            float a = 4;
+            return a + 4.6864655f;// jeigu yra nera void yra return
+        }
+
+        public static void IsveskKazkaEkranan()
+        {
+
+            Console.WriteLine("Isvedu kazka");
+        }
+
+
+    }
+}
