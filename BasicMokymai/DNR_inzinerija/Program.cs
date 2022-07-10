cfddf8067e3c947253dd2110c3b0f26ecada207b
MatasVal <valauskasmatas@gmail.com>
2022-07-10 22:19:53 +0300

    submeniu

--- BasicMokymai/DNR_inzinerija/Program.cs
+++ BasicMokymai/DNR_inzinerija/Program.cs
@@ -7,16 +7,7 @@
             var pradinisDNR = " T CG-TAC- gaC-TAC-CGT-CAG-ACT-TAa-CcA-GTC-cAt-AGA-GCT    ";
             Console.WriteLine($"DNR grandinė: {pradinisDNR}");
             Console.WriteLine();
-            Console.WriteLine(@"Kokį veiksmą norite atlikti?
-
-          1. Atlikti DNR grandinės normalizavimo veiksmus
-             
-          2. Atlikti grandinės validaciją
-             
-          3. Atlikti veiksmus su DNR grandine (tik tuo atveju jei grandinė yra normalizuota ir validi)");
-            var pasirinkimas0 = Console.ReadLine();
-
-            Console.WriteLine();
+            Meniu(pradinisDNR);
             Console.WriteLine($"Normalizuota grandine: {NormalizavimasGrandines(pradinisDNR)}");
             Console.WriteLine();
             Console.WriteLine($"Ar validi grandine: {ValidacijaGrandines(pradinisDNR)}");
@@ -29,12 +20,16 @@
           1. Atlikti DNR grandinės normalizavimo veiksmus:
              - pašalina tarpus.
              - visas raides keičia didžiosiomis.
+
           2. Atlikti grandinės validaciją
              - patikrina ar nėra kitų nei ATCG raidžių
+
           3. Atlieka veiksmus su DNR grandine (tik tuo atveju jei grandinė yra normalizuota ir validi). Nuspaudus 3 įeinama į sub-meniu
+
               - Jeigu grandinė yra validi, tačiau nenormalizuota programa pasiūlo naudotojui
               1) normalizuoti grandinę
               2) išeiti iš programos
+
               - jei grandinė normalizuota arba kai buvo atlikta normalizacija
               1) GCT pakeis į AGG
               2) Išvesti ar yra tekste CAT
@@ -47,6 +42,7 @@
               8) Pakeisti pasirinkti segmentą įvestu iš klaviatūros
                   (reikalinga validacija ar nėra kitų kaip ATCG ir 3 raidės)
               9) Gryžti į ankstesnį meniu
+
         Visoms operacijoms reikalingi testai.
         */
 
@@ -60,7 +56,37 @@
            Console.WriteLine($"ilgis: {ilgisPasalinusATCG}");
            return ilgisPasalinusATCG == 0 ? "Validi" : "Nevalidi";
         }
-        public static void VeiksmaiSuGrandine()
+        public static void Meniu(string pradinisDNR)
+        {
+            Console.WriteLine(@"Kokį veiksmą norite atlikti?
+
+          1. Atlikti DNR grandinės normalizavimo veiksmus
+             
+          2. Atlikti grandinės validaciją
+             
+          3. Atlikti veiksmus su DNR grandine (tik tuo atveju jei grandinė yra normalizuota ir validi)");
+
+            int.TryParse(Console.ReadLine(), out int pasirinkimas0);
+
+            switch (pasirinkimas0)
+            {
+                case 1: 
+                    pradinisDNR = NormalizavimasGrandines(pradinisDNR);
+                    break;
+                case 2:
+                    pradinisDNR = ValidacijaGrandines(pradinisDNR);
+                    break;
+                case 3:
+                    SubMeniu(pradinisDNR);
+                    break;
+                default:
+                    Console.Clear();
+                    Console.WriteLine("Tokio pasirinkimo nera");
+                    break;
+            }
+
+        }
+        public static void SubMeniu(string pradinisDNR)
         {
 
         }
